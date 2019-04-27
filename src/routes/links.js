7f61b0a0d88703bbbb4a120d4ac75507f6b4afd6
Kleyton João <kleyton.joao@gmail.com>
2019-04-27 00:37:32 -0300

    Criando pagina de links e configurando rotas

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const db = require('../database')
 
+router.get('/add', (req, res) => {
+    res.render('links/add')
+});
+
+router.post('/add', (req, res)=>{
+    res.send("Recebido");
+});
+
 module.exports = router
